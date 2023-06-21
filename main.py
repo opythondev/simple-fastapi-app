94dab1fbb2e9172057a4cbb9cace0113ad511c16
Sergey <115350236+opythondev@users.noreply.github.com>
2023-06-21 10:03:31 +0300

    feat(main.py) basic methods impl

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
 from fastapi import FastAPI
 
+fake_users = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "David"},
+    {"id": 3, "role": "trader", "name": "John"},
+]
+
+fake_users2 = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "David"},
+    {"id": 3, "role": "trader", "name": "John"},
+]
+
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "ammount": 2.12},
+    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "ammount": 2.12},
+]
+
 app = FastAPI(title="Trading App")
+
+
+@app.get("/users/{user_id}")
+async def get_users(user_id: int):
+    return [user for user in fake_users if user.get("id") == user_id]
+
+
+@app.get("/trades")
+async def get_trades(limit: int = 1, offset: int = 0):
+    return fake_trades[offset:][:limit]
+
+
+@app.post("/users/{user_id}")
+async def change_user_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
+    current_user["name"] = new_name
+    return {"status": 200, "data": current_user}
